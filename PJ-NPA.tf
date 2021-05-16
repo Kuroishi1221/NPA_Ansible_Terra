af86f90146522b024ffe721809c6a7cd6545b5f0
Phakapol <42952006+Phakapol@users.noreply.github.com>
2021-05-16 15:51:41 +0700

    Update PJ-NPA.tf

--- PJ-NPA.tf
+++ PJ-NPA.tf
@@ -412,3 +412,24 @@ value = "ssh -i vockey.pem ec2-user@${aws_instance.db1.public_ip}"
 output "aws_instance_public_ip_ansibleNode" {
 value = "ssh -i vockey.pem ec2-user@${aws_instance.web.public_ip}"
 }
+
+
+
+output "aws_instance_private_ip_web2" {
+value = aws_instance.web2.private_ip
+}
+
+output "aws_instance_public_ip_web2" {
+value = "ssh -i vockey.pem ec2-user@${aws_instance.web2.public_ip}"
+}
+
+output "aws_instance_private_ip_db2" {
+value = aws_instance.db2.private_ip
+}
+output "aws_instance_public_ip_db2" {
+value = "ssh -i vockey.pem ec2-user@${aws_instance.db2.public_ip}"
+}
+
+output "aws_instance_public_ip_ansibleNode" {
+value = "ssh -i vockey.pem ec2-user@${aws_instance.web2.public_ip}"
+}
